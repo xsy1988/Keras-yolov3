bc6f189685c94955ac4a3e88400fd43eb7946400
xsy1988 <weilaixsy@126.com>
2019-08-16 16:29:10 +0800

    Update detect_img.py

--- detect_img.py
+++ detect_img.py
@@ -8,15 +8,15 @@ def detect_img(yolo, image):
     r_img.show()
     yolo.close_session()
 
-model_path = "/Users/weilaixsy/python/yolo_v3/logs/trained_weights_final.h5"
-anchors_path = "/Users/weilaixsy/python/yolo_v3/data_set/annotation/yolo_clusters.txt"
-classes_path = "/Users/weilaixsy/python/yolo_v3/data_set/annotation/classes.txt"
+model_path = "/logs/trained_weights_final.h5"
+anchors_path = "/data_set/annotation/yolo_clusters.txt"
+classes_path = "/data_set/annotation/classes.txt"
 model_image_size = (416, 416)
 gpu_num = 1
 
 if __name__ == '__main__':
 
     yolo = YOLO(model_path=model_path, anchors_path=anchors_path, classes_path=classes_path, model_image_size=model_image_size)
-    image_path = "/Users/weilaixsy/python/yolo_v3/data_set/test_img/test1.jpg"
+    image_path = "/data_set/test_img/test1.jpg"
     image = Image.open(image_path)
     detect_img(yolo, image)
